0525228ad37288ef6f5e6453f5b3dee4fb18415b
Scott Wilkins <scottwilkins@Scotts-mac.local>
2016-12-01 12:11:50 -0700

    set up routes

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import PostsIndex from './components/posts_index'
+
+import App from './components/app';
+
+const Greeting = () => {
+  return <div>hey good looking!</div>
+};
+
+export default (
+  <Route path="/" component={App} >
+    <IndexRoute component={PostsIndex} />
+    <Route path="greet" component={Greeting} />
+  </Route>
+);
